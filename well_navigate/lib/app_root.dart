1f72baa0a714ea6fa021b88ae5c825d97b1870a4
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2024-05-27 16:35:32 +1000

    Theming

--- well_navigate/lib/app_root.dart
+++ well_navigate/lib/app_root.dart
@@ -4,6 +4,27 @@ import 'package:flutter/material.dart';
 import 'package:well_navigate/main.dart';
 import 'package:well_navigate/resource_editor.dart';
 
+final themeData = ThemeData(
+  colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF81B7A1)),
+  fontFamily: 'Roboto',
+  inputDecorationTheme: InputDecorationTheme(
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8),
+      borderSide: BorderSide(color: const Color(0xFF81B7A1).withOpacity(0.5)),
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8),
+      borderSide: BorderSide(color: const Color(0xFF81B7A1).withOpacity(0.5)),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8),
+      borderSide: const BorderSide(color: Color(0xFF4D8C7B)),
+    ),
+    labelStyle: TextStyle(color: const Color(0xFF81B7A1).withOpacity(0.8)),
+  ),
+  useMaterial3: true,
+);
+
 class AppRoot extends StatelessWidget {
   const AppRoot({super.key});
 
@@ -11,26 +32,7 @@ class AppRoot extends StatelessWidget {
   Widget build(BuildContext context) => MaterialApp(
         title: 'Well Navigator',
         debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-          fontFamily: 'Roboto',
-          inputDecorationTheme: InputDecorationTheme(
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8),
-              borderSide: const BorderSide(color: Colors.grey),
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8),
-              borderSide: const BorderSide(color: Colors.grey),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8),
-              borderSide: const BorderSide(color: Colors.blue),
-            ),
-            labelStyle: const TextStyle(color: Colors.grey),
-          ),
-          useMaterial3: true,
-        ),
+        theme: themeData,
         home: Scaffold(
           body: ResourceEditor(
             resource: appointment,
