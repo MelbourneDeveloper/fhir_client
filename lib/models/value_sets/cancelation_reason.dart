a5dd26cb9165cc6f3224fdc3c241f3a3c0c8d580
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2024-05-29 07:14:47 +1000

    fix up the value set

--- lib/models/value_sets/cancelation_reason.dart
+++ lib/models/value_sets/cancelation_reason.dart
@@ -1,18 +1,14 @@
 // ignore_for_file: lines_longer_than_80_chars
 
-import 'package:jayse/jayse.dart';
-
-const cancelationReasonUri =
-    'http://terminology.hl7.org/CodeSystem/appointment-cancellation-reason';
+import 'package:fhir_client/models/value_sets/value_set_concept.dart';
 
 /// The coded reason for the appointment being canceled.
-enum CancelationReason implements Comparable<CancelationReason> {
+enum CancelationReason with ValueSetConcept<CancelationReason> {
   /// The patient has cancelled the appointment.
   patientCancel(
     code: 'pat',
     display: 'Patient',
     definition: 'The patient has cancelled the appointment.',
-    system: cancelationReasonUri,
   ),
 
   /// The practitioner has cancelled the appointment.
@@ -20,7 +16,6 @@ enum CancelationReason implements Comparable<CancelationReason> {
     code: 'prac',
     display: 'Practitioner',
     definition: 'The practitioner has cancelled the appointment.',
-    system: cancelationReasonUri,
   ),
 
   /// The location/system has cancelled the appointment.
@@ -28,7 +23,6 @@ enum CancelationReason implements Comparable<CancelationReason> {
     code: 'loc',
     display: 'Location',
     definition: 'The location/system has cancelled the appointment.',
-    system: cancelationReasonUri,
   ),
 
   /// The appointment has been cancelled by the organization.
@@ -36,7 +30,6 @@ enum CancelationReason implements Comparable<CancelationReason> {
     code: 'org',
     display: 'Organization',
     definition: 'The appointment has been cancelled by the organization.',
-    system: cancelationReasonUri,
   ),
 
   /// The appointment has been cancelled and will be rebooked or rescheduled.
@@ -45,7 +38,6 @@ enum CancelationReason implements Comparable<CancelationReason> {
     display: 'Maintenance',
     definition:
         'The appointment has been cancelled and will be rebooked or rescheduled.',
-    system: cancelationReasonUri,
   ),
 
   /// The appointment has been cancelled as the patient did not show up.
@@ -54,20 +46,22 @@ enum CancelationReason implements Comparable<CancelationReason> {
     display: 'No Show',
     definition:
         'The appointment has been cancelled as the patient did not show up.',
-    system: cancelationReasonUri,
   );
 
   const CancelationReason({
     required this.code,
     required this.display,
     required this.definition,
-    required this.system,
   });
 
+  @override
   final String code;
+
+  @override
   final String display;
+
+  @override
   final String definition;
-  final String system;
 
   /// Returns the cancelation reason based on the string code, and returns null if
   /// no match is found
@@ -80,9 +74,4 @@ enum CancelationReason implements Comparable<CancelationReason> {
         ('nosh') => CancelationReason.noShowCancel,
         (_) => null,
       };
-
-  JsonValue get json => JsonString(code);
-
-  @override
-  int compareTo(CancelationReason other) => code == other.code ? 0 : 1;
 }
