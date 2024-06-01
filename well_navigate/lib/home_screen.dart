d4db365de1d214722230a39dd7b0e05f1ba94a97
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2024-06-01 15:51:28 +1000

    Break stuff up more

--- well_navigate/lib/home_screen.dart
+++ well_navigate/lib/home_screen.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:well_navigate/constants.dart';
-import 'package:well_navigate/main_app_bar.dart';
-import 'package:well_navigate/menu.dart';
 import 'package:well_navigate/mock_data.dart';
 import 'package:well_navigate/resource_editor/resource_editor.dart';
 
@@ -9,16 +6,8 @@ class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: mainAppBar(
-          'Appointment',
-          iconsByResourceType['Appointment'] ?? Icons.medical_services,
-          context,
-        ),
-        body: ResourceEditor(
-          resourceRoot: appointment.json,
-          resourceTypeName: 'Appointment',
-        ),
-        drawer: const Menu(),
+  Widget build(BuildContext context) => ResourceEditor(
+        resourceRoot: appointment.json,
+        resourceTypeName: 'Appointment',
       );
 }
