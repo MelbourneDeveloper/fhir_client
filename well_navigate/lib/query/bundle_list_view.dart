72a4bd0359b63d6322a8d18de3e4a4e0ddff682b
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2024-06-01 15:16:33 +1000

    Display more stuff

--- well_navigate/lib/query/bundle_list_view.dart
+++ well_navigate/lib/query/bundle_list_view.dart
@@ -1,6 +1,8 @@
 import 'package:fhir_client/models/basic_types/fixed_list.dart';
 import 'package:fhir_client/models/entry.dart';
+import 'package:fhir_client/models/resource.dart';
 import 'package:flutter/material.dart';
+import 'package:well_navigate/constants.dart';
 
 class BundleListView extends StatelessWidget {
   const BundleListView({
@@ -13,15 +15,30 @@ class BundleListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) => ListView.builder(
         itemCount: entries.length,
-        itemBuilder: (context, index) => ListTile(
-          title: Text(
-            entries[index].resource?.resourceType ?? '',
-            style: Theme.of(context).textTheme.titleMedium,
-          ),
-          subtitle: Text(
-            'Id: ${entries[index].resource?.id ?? ''}',
-            style: Theme.of(context).textTheme.bodyMedium,
-          ),
-        ),
+        itemBuilder: (context, index) => switch (entries[index].resource) {
+          (final Resource resource) => ExpansionTile(
+              title: Text(
+                '${resource.resourceType} - ${resource.id}',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              children: fieldDefinitionsByElementType[resource.resourceType]!
+                  .map((fieldDefinition) {
+                final fieldName = fieldDefinition.name;
+                final fieldValue = fieldDefinition.getValue(resource.json);
+
+                return ListTile(
+                  title: Text(
+                    fieldName,
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                  subtitle: Text(
+                    fieldValue.toString(),
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                );
+              }).toList(),
+            ),
+          _ => const Text('Not a resource')
+        },
       );
 }
