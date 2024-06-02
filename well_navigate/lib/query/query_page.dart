f65ca879fc1a117c72ec70b70d9860cb5a8988e7
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2024-06-02 15:01:30 +1000

    Display HTML divs

--- well_navigate/lib/query/query_page.dart
+++ well_navigate/lib/query/query_page.dart
@@ -2,6 +2,7 @@ import 'package:fhir_client/models/basic_types/fixed_list.dart';
 import 'package:fhir_client/models/resource.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ioc_container/flutter_ioc_container.dart';
+import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
 import 'package:well_navigate/query/bundle_list_view.dart';
 import 'package:well_navigate/query/query_notifier.dart';
 import 'package:well_navigate/query/url_picker.dart';
@@ -42,15 +43,15 @@ class _QueryPageState extends State<QueryPage> {
             child: const Text('Send Request'),
           ),
           const SizedBox(height: 20),
-          const Text('Response:'),
           Expanded(
             child: switch (context<QueryNotifier>()) {
               QueryNotifier(isLoading: true) => const Center(
                   child: CircularProgressIndicator(),
                 ),
               _ => switch (context<QueryNotifier>().queryResult) {
-                  (final OperationOutcome<dynamic> oo) =>
-                    Text(oo.text?.div ?? ''),
+                  (final OperationOutcome<dynamic> oo) => SingleChildScrollView(
+                      child: HtmlWidget(oo.text?.div ?? '<div>Error</div>'),
+                    ),
                   (final Bundle bundle)
                       when bundle.entry?.isNotEmpty ?? false =>
                     BundleListView(entries: bundle.entry!.toFixedList()),
