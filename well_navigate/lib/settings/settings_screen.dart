7a0de77a09e8072b32685cc46558007788c0017d
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2024-06-02 19:21:28 +1000

    Center

--- well_navigate/lib/settings/settings_screen.dart
+++ well_navigate/lib/settings/settings_screen.dart
@@ -16,22 +16,26 @@ class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) => ValueListenableBuilder(
         valueListenable: _controller,
-        builder: (context, settings, child) => Center(
-          child: ConstrainedBox(
-            constraints: const BoxConstraints(maxWidth: 500),
-            child: ListView(
-              children: [
-                SwitchListTile(
-                  title: const Text('Dark Mode'),
-                  value: settings?.isDarkMode ?? false,
-                  onChanged: (_) async => _controller.toggleDarkMode(),
-                ),
-                ListTile(
-                  title: const Text('Base Url'),
-                  subtitle:
-                      TextField(controller: _controller.baseUriController),
-                ),
-              ],
+        builder: (context, settings, child) => Scaffold(
+          body: Center(
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(maxWidth: 500),
+              child: ListView(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                children: [
+                  SwitchListTile(
+                    title: const Text('Dark Mode'),
+                    value: settings?.isDarkMode ?? false,
+                    onChanged: (_) async => _controller.toggleDarkMode(),
+                  ),
+                  ListTile(
+                    title: const Text('Base Url'),
+                    subtitle:
+                        TextField(controller: _controller.baseUriController),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
