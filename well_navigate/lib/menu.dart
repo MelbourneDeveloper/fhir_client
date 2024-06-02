ae8a33a5ad8a648edadf2e448f80a6f4078271c1
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2024-06-03 08:52:39 +1000

    Refactoring

--- well_navigate/lib/menu.dart
+++ well_navigate/lib/menu.dart
@@ -89,53 +89,66 @@ List<MenuItem> flattenMenuElements(List<MenuElement> elements) =>
 
 final menuItems = flattenMenuElements(menuElements);
 
-class Menu extends StatelessWidget {
+class Menu extends StatefulWidget {
   const Menu({super.key});
 
   @override
+  State<Menu> createState() => _MenuState();
+}
+
+class _MenuState extends State<Menu> {
+  int? _selectedIndex;
+
+  @override
   Widget build(BuildContext context) => NavigationDrawer(
+        selectedIndex: _selectedIndex,
         tilePadding: const EdgeInsets.all(4),
-        onDestinationSelected: (index) async => Navigator.pushNamed(
-          context,
-          menuItems[index].routeName,
-          arguments: menuItems[index].arguments,
-        ),
-        children: menuElements.expand<Widget>((element) {
-          if (element is MenuGroup) {
-            return [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(28, 16, 16, 10),
-                child: Text(
-                  element.name,
-                  style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
+        onDestinationSelected: (index) async {
+          setState(() => _selectedIndex = index);
+          await Navigator.pushNamed(
+            context,
+            menuItems[index].routeName,
+            arguments: menuItems[index].arguments,
+          );
+        },
+        children: _menuElementWidgets(),
+      );
+
+  List<Widget> _menuElementWidgets() => menuElements.expand<Widget>((element) {
+        if (element is MenuGroup) {
+          return [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(28, 16, 16, 10),
+              child: Text(
+                element.name,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              ...element.children.expand<Widget>((child) {
-                if (child is MenuItem) {
-                  return [
-                    _navigationDestination(child),
-                  ];
-                } else if (child is MenuDivider) {
-                  return [const Divider()];
-                } else {
-                  return [];
-                }
-              }),
-            ];
-          } else if (element is MenuItem) {
-            return [
-              _navigationDestination(element),
-            ];
-          } else if (element is MenuDivider) {
-            return [const Divider()];
-          } else {
-            return [];
-          }
-        }).toList(),
-      );
+            ),
+            ...element.children.expand<Widget>((child) {
+              if (child is MenuItem) {
+                return [
+                  _navigationDestination(child),
+                ];
+              } else if (child is MenuDivider) {
+                return [const Divider()];
+              } else {
+                return [];
+              }
+            }),
+          ];
+        } else if (element is MenuItem) {
+          return [
+            _navigationDestination(element),
+          ];
+        } else if (element is MenuDivider) {
+          return [const Divider()];
+        } else {
+          return [];
+        }
+      }).toList();
 
   NavigationDrawerDestination _navigationDestination(MenuItem child) =>
       NavigationDrawerDestination(
