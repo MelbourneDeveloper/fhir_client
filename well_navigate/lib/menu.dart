e6bafd63687f5c99dd3ac730ed970d9cad9e5bfb
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2024-06-03 08:40:13 +1000

    refactor the menu

--- well_navigate/lib/menu.dart
+++ well_navigate/lib/menu.dart
@@ -1,124 +1,149 @@
 import 'package:flutter/material.dart';
 import 'package:well_navigate/about_screen.dart';
-import 'package:well_navigate/constants.dart';
 import 'package:well_navigate/query/query_page.dart';
 import 'package:well_navigate/settings/settings_screen.dart';
 
-const destinations = <String,
-    ({
-  String routeName,
-  Object? arguments,
-})>{
-  'patient': (
-    routeName: QueryPage.routeName,
-    arguments: '/Patient',
-  ),
-  'appointment': (
-    routeName: QueryPage.routeName,
-    arguments: '/Appointment',
-  ),
-  'query': (
-    routeName: QueryPage.routeName,
-    arguments: null,
-  ),
-  'settings': (
-    routeName: SettingsScreen.routeName,
-    arguments: null,
-  ),
+/// An element in the menu
+sealed class MenuElement {
+  const MenuElement();
+}
 
-  'about': (
+/// A group of menu elements so elements can be organized
+/// into a hierarchy
+final class MenuGroup extends MenuElement {
+  const MenuGroup(this.children, {required this.name});
+
+  final String name;
+  final List<MenuElement> children;
+}
+
+/// A menu item that navigates to a route
+final class MenuItem extends MenuElement {
+  const MenuItem(
+    this.name,
+    this.icon, {
+    required this.routeName,
+    required this.arguments,
+  }) : super();
+
+  final String routeName;
+  final Object? arguments;
+  final String name;
+  final IconData icon;
+}
+
+/// A divider in the menu
+final class MenuDivider extends MenuElement {
+  const MenuDivider() : super();
+}
+
+const menuElements = <MenuElement>[
+  MenuItem(
+    'About',
+    Icons.info,
     routeName: AboutScreen.routeName,
     arguments: null,
   ),
-  // 'observation': '/observation',
-  // 'medication': '/medication',
-};
+  MenuItem(
+    'Settings',
+    Icons.settings,
+    routeName: SettingsScreen.routeName,
+    arguments: null,
+  ),
+  MenuDivider(),
+  MenuGroup(
+    [
+      MenuItem(
+        'Query',
+        Icons.search,
+        routeName: QueryPage.routeName,
+        arguments: null,
+      ),
+      MenuItem(
+        'Patients',
+        Icons.person,
+        routeName: QueryPage.routeName,
+        arguments: '/Patient',
+      ),
+      MenuItem(
+        'Appointments',
+        Icons.calendar_today,
+        routeName: QueryPage.routeName,
+        arguments: '/Appointment',
+      ),
+    ],
+    name: 'Queries',
+  ),
+];
+
+List<MenuItem> flattenMenuElements(List<MenuElement> elements) =>
+    elements.expand<MenuItem>((element) {
+      if (element is MenuGroup) {
+        return flattenMenuElements(element.children);
+      } else if (element is MenuItem) {
+        return [element];
+      } else {
+        return [];
+      }
+    }).toList();
+
+final menuItems = flattenMenuElements(menuElements);
 
 class Menu extends StatelessWidget {
   const Menu({super.key});
 
   @override
   Widget build(BuildContext context) => NavigationDrawer(
-        onDestinationSelected: (index) async {
-          final key = destinations.keys.elementAt(index);
-          final destination = destinations[key]!;
+        tilePadding: const EdgeInsets.all(4),
+        onDestinationSelected: (index) async => Navigator.pushNamed(
+          context,
+          menuItems[index].routeName,
+          arguments: menuItems[index].arguments,
+        ),
+        children: menuElements.expand<Widget>((element) {
+          if (element is MenuGroup) {
+            return [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(28, 16, 16, 10),
+                child: Text(
+                  element.name,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              ...element.children.expand<Widget>((child) {
+                if (child is MenuItem) {
+                  return [
+                    _navigationDestination(child),
+                  ];
+                } else if (child is MenuDivider) {
+                  return [const Divider()];
+                } else {
+                  return [];
+                }
+              }),
+            ];
+          } else if (element is MenuItem) {
+            return [
+              _navigationDestination(element),
+            ];
+          } else if (element is MenuDivider) {
+            return [const Divider()];
+          } else {
+            return [];
+          }
+        }).toList(),
+      );
 
-          await switch (key) {
-            'patient' || 'appointment' => Navigator.pushNamed(
-                context,
-                destination.routeName,
-                arguments: destination.arguments,
-              ),
-            'observation' ||
-            'medication' ||
-            'query' ||
-            'settings' ||
-            'about' =>
-              Navigator.pushNamed(context, destination.routeName),
-            _ => null,
-          };
-        },
-        children: <Widget>[
-          const Padding(
-            padding: EdgeInsets.fromLTRB(28, 16, 16, 10),
-            child: Text(
-              'FHIR Resources',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          NavigationDrawerDestination(
-            icon: Icon(iconsByResourceType['Patient']),
-            label: const Text('Patients'),
-          ),
-          NavigationDrawerDestination(
-            icon: Icon(iconsByResourceType['Appointment']),
-            label: const Text('Appointments'),
-          ),
-          // NavigationDrawerDestination(
-          //   icon: Icon(iconsByResourceType['Observations']),
-          //   label: const Text('Observation'),
-          // ),
-          // NavigationDrawerDestination(
-          //   icon: Icon(iconsByResourceType['Medications']),
-          //   label: const Text('Medication'),
-          // ),
-          const Divider(),
-          const Padding(
-            padding: EdgeInsets.fromLTRB(28, 16, 16, 10),
-            child: Text(
-              'Tools',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          const NavigationDrawerDestination(
-            icon: Icon(Icons.search),
-            label: Text('Query'),
-          ),
-          const Divider(),
-          const Padding(
-            padding: EdgeInsets.fromLTRB(28, 16, 16, 10),
-            child: Text(
-              'App',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          const NavigationDrawerDestination(
-            icon: Icon(Icons.settings),
-            label: Text('Settings'),
-          ),
-          const NavigationDrawerDestination(
-            icon: Icon(Icons.info),
-            label: Text('About'),
-          ),
-        ],
+  NavigationDrawerDestination _navigationDestination(MenuItem child) =>
+      NavigationDrawerDestination(
+        icon: Icon(
+          child.icon,
+        ),
+        label: Text(
+          child.name,
+        ),
       );
 }
