35d4b281cc7e17f28896b55a40cadf6952aef519
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2024-06-06 21:08:47 +1000

    Cleanup

--- well_navigate/lib/editors/value_set_editor.dart
+++ well_navigate/lib/editors/value_set_editor.dart
@@ -2,6 +2,7 @@ import 'package:fhir_client/models/value_sets/value_set_concept.dart';
 import 'package:fhir_client/validation/field_definition.dart';
 import 'package:flutter/material.dart';
 import 'package:well_navigate/helpers/iterable_extensions.dart';
+import 'package:well_navigate/ui_functions.dart';
 
 class ValueSetEditor<T> extends StatelessWidget {
   const ValueSetEditor({
@@ -16,28 +17,10 @@ class ValueSetEditor<T> extends StatelessWidget {
   @override
   Widget build(BuildContext context) => DropdownButtonHideUnderline(
         child: DropdownButtonFormField<ValueSetConcept>(
-          decoration: InputDecoration(
-            labelText: fieldDefinition.display ?? fieldDefinition.name,
-            border: const OutlineInputBorder(),
+          decoration: standardInputDecoration(
+            fieldDefinition.display ?? fieldDefinition.name,
           ),
-          selectedItemBuilder: (context) => fieldDefinition.valueSetValues!
-              .cast<ValueSetConcept>()
-              .toSortedList((a, b) => a.compareTo(b))
-              .map(
-                (l) => Tooltip(
-                  message: l.definition,
-                  child: ConstrainedBox(
-                    constraints: const BoxConstraints(
-                      maxWidth: 200,
-                    ), // Adjust the maxWidth value
-                    child: Text(
-                      '${l.code} - ${l.display}',
-                      overflow: TextOverflow.fade,
-                    ),
-                  ),
-                ),
-              )
-              .toList(),
+
           items: fieldDefinition.valueSetValues!
               .cast<ValueSetConcept>()
               .toSortedList((a, b) => a.compareTo(b))
